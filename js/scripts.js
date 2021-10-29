65e3b3b3010448873f4863e246166029808bf8a7
Kim Brannian <brannian@gmail.com>
2021-10-29 16:02:50 -0700

    Pass test 2

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,14 @@ for (i=1; i<=userInput; i++) {
 }
 console.log(beepBoop)
 
-
+const userInput = 1;
+let beepBoop = []
+for (beepBoop(element)(i=1); i<=userInput; i++) {
+    if(element === 1) {
+        return = "beep";
+    };
+    console.log(beepBoop)
+};
 
 
 
